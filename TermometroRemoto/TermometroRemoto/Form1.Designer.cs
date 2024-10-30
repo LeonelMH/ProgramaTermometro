604c5d8dc3218a87289b7c3dbcb397272357a3d9
Leonel Martinez Huitron <136146406+LeonelMH@users.noreply.github.com>
2024-10-30 16:49:53 -0700

    Add files via upload

---
+++ TermometroRemoto/TermometroRemoto/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace TermometroRemoto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTemperatura = new System.Windows.Forms.Label();
+            this.labelEstado = new System.Windows.Forms.Label();
+            this.buttonConectar = new System.Windows.Forms.Button();
+            this.buttonDesconectar = new System.Windows.Forms.Button();
+            this.comboBoxPorts = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // labelTemperatura
+            // 
+            this.labelTemperatura.AutoSize = true;
+            this.labelTemperatura.Location = new System.Drawing.Point(21, 18);
+            this.labelTemperatura.Name = "labelTemperatura";
+            this.labelTemperatura.Size = new System.Drawing.Size(73, 13);
+            this.labelTemperatura.TabIndex = 0;
+            this.labelTemperatura.Text = "Temperatura: ";
+            // 
+            // labelEstado
+            // 
+            this.labelEstado.AutoSize = true;
+            this.labelEstado.Location = new System.Drawing.Point(21, 42);
+            this.labelEstado.Name = "labelEstado";
+            this.labelEstado.Size = new System.Drawing.Size(30, 13);
+            this.labelEstado.TabIndex = 1;
+            this.labelEstado.Text = "--  °C";
+            // 
+            // buttonConectar
+            // 
+            this.buttonConectar.Location = new System.Drawing.Point(154, 59);
+            this.buttonConectar.Name = "buttonConectar";
+            this.buttonConectar.Size = new System.Drawing.Size(85, 23);
+            this.buttonConectar.TabIndex = 2;
+            this.buttonConectar.Text = "Conectar";
+            this.buttonConectar.UseVisualStyleBackColor = true;
+            this.buttonConectar.Click += new System.EventHandler(this.buttonConectar_Click);
+            // 
+            // buttonDesconectar
+            // 
+            this.buttonDesconectar.Location = new System.Drawing.Point(154, 109);
+            this.buttonDesconectar.Name = "buttonDesconectar";
+            this.buttonDesconectar.Size = new System.Drawing.Size(85, 23);
+            this.buttonDesconectar.TabIndex = 3;
+            this.buttonDesconectar.Text = "Desconectar";
+            this.buttonDesconectar.UseVisualStyleBackColor = true;
+            this.buttonDesconectar.Click += new System.EventHandler(this.buttonDesconectar_Click);
+            // 
+            // comboBoxPorts
+            // 
+            this.comboBoxPorts.FormattingEnabled = true;
+            this.comboBoxPorts.Location = new System.Drawing.Point(134, 18);
+            this.comboBoxPorts.Name = "comboBoxPorts";
+            this.comboBoxPorts.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPorts.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 191);
+            this.Controls.Add(this.comboBoxPorts);
+            this.Controls.Add(this.buttonDesconectar);
+            this.Controls.Add(this.buttonConectar);
+            this.Controls.Add(this.labelEstado);
+            this.Controls.Add(this.labelTemperatura);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTemperatura;
+        private System.Windows.Forms.Label labelEstado;
+        private System.Windows.Forms.Button buttonConectar;
+        private System.Windows.Forms.Button buttonDesconectar;
+        private System.Windows.Forms.ComboBox comboBoxPorts;
+    }
+}
+
---
+++ TermometroRemoto/TermometroRemoto/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace TermometroRemoto
+{
+    public partial class Form1 : Form
+    {
+        private SerialPort _serialPort;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeSerialPort();
+        }
+
+        private void InitializeSerialPort()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.BaudRate = 9600;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.DataBits = 8;
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+            // Opcional: Lista de puertos disponibles
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                comboBoxPorts.Items.Add(port);
+            }
+
+            if (comboBoxPorts.Items.Count > 0)
+                comboBoxPorts.SelectedIndex = 0;
+        }
+
+        private void buttonConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _serialPort.PortName = comboBoxPorts.SelectedItem.ToString();
+                _serialPort.Open();
+                labelEstado.Text = "Estado: Conectado";
+                buttonConectar.Enabled = false;
+                buttonDesconectar.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el puerto serial: " + ex.Message);
+            }
+        }
+
+        private void buttonDesconectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _serialPort.Close();
+                labelEstado.Text = "Estado: Desconectado";
+                buttonConectar.Enabled = true;
+                buttonDesconectar.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cerrar el puerto serial: " + ex.Message);
+            }
+        }
+
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string data = _serialPort.ReadLine().Trim();
+                this.BeginInvoke(new Action(() =>
+                {
+                    labelTemperatura.Text = data + " °C";
+                }));
+            }
+            catch (Exception)
+            {
+                // Manejo de errores opcional
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_serialPort.IsOpen)
+                _serialPort.Close();
+        }
+    }
+}
